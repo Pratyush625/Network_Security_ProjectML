aaa95dbea972ff4727b01fa7dbd7ba09d58906c4
praty <pratyush625@gmail.com>
2024-08-17 21:57:22 +0530

    set.py and requirements.txt updated

---
+++ setup.py
@@ -0,0 +1,8 @@
+setup(
+    name="networksecurity",
+    version="0.0.1",
+    author="sunny savita",
+    author_email="snshrivas3365@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements()
+)
